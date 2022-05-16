3b3b2f9a9e3b08c38a2beb2a5c495294f15753b3
Dima Shtrikker <shtrikker28@gmail.com>
2022-05-16 22:22:25 +0300

    Add exception catch

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -40,103 +40,113 @@ void EditMatrix(Matrix& matrix)
                  ch != 3 && ch != 4 && ch != 5 &&
                  ch != 6 && ch != 7 && ch != 8 &&
                  ch != 9 && ch != 10 && ch != 11);
-        switch (ch)
+        try
         {
-        case (1):
+            switch (ch)
             {
-                std::cout << "Determinant: " << matrix.determinant() << std::endl;
-                std::cout << "Press any key";
-                _getch();
+            case (1):
+                {
+                    std::cout << "Determinant: " << matrix.determinant() << std::endl;
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (2):
+                {
+                    std::cout << "Rang: " << matrix.rang() << std::endl;
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (3):
+                {
+                    auto answers = matrix.gauss();
+                    std::cout << "Answers (gauss method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (4):
+                {
+                    std::cout << "Action course:" << std::endl;
+                    auto answers = matrix.gauss(true);
+                    std::cout << "Answers (gauss method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (5):
+                {
+                    auto answers = matrix.cramera();
+                    std::cout << "Answers (cramera method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (6):
+                {
+                    std::cout << "Action course:" << std::endl;
+                    auto answers = matrix.cramera(true);
+                    std::cout << "Answers (cramera method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (7):
+                {
+                    auto answers = matrix.montane();
+                    std::cout << "Answers (montane method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (8):
+                {
+                    std::cout << "Action course:" << std::endl;
+                    auto answers = matrix.montane(true);
+                    std::cout << "Answers (montane method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (9):
+                {
+                    auto answers = matrix.matrixMethod();
+                    std::cout << "Answers (matrix method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (10):
+                {
+                    std::cout << "Action course:" << std::endl;
+                    auto answers = matrix.matrixMethod(true);
+                    std::cout << "Answers (matrix method): " << std::endl;
+                    PrintAnswers(answers);
+                    std::cout << "Press any key";
+                    _getch();
+                }
+                break;
+            case (11):
+                {
+                    std::cin >> matrix;
+                }
+                break;
             }
-            break;
-        case (2):
-            {
-                std::cout << "Rang: " << matrix.rang() << std::endl;
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (3):
-            {
-                auto answers = matrix.gauss();
-                std::cout << "Answers (gauss method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (4):
-            {
-                std::cout << "Action course:" << std::endl;
-                auto answers = matrix.gauss(true);
-                std::cout << "Answers (gauss method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (5):
-            {
-                auto answers = matrix.cramera();
-                std::cout << "Answers (cramera method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (6):
-            {
-                std::cout << "Action course:" << std::endl;
-                auto answers = matrix.cramera(true);
-                std::cout << "Answers (cramera method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (7):
-            {
-                auto answers = matrix.montane();
-                std::cout << "Answers (montane method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (8):
-            {
-                std::cout << "Action course:" << std::endl;
-                auto answers = matrix.montane(true);
-                std::cout << "Answers (montane method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (9):
-            {
-                auto answers = matrix.matrixMethod();
-                std::cout << "Answers (matrix method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (10):
-            {
-                std::cout << "Action course:" << std::endl;
-                auto answers = matrix.matrixMethod(true);
-                std::cout << "Answers (matrix method): " << std::endl;
-                PrintAnswers(answers);
-                std::cout << "Press any key";
-                _getch();
-            }
-            break;
-        case (11):
-            {
-                std::cin >> matrix;
-            }
-            break;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl
+                      << "Try another method or change matrix" << std::endl
+                      << "Press any key";
+            _getch();
         }
     }
 }
